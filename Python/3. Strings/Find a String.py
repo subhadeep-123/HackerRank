872edf229434ef991682cead41b9c76021ded22b
Subhadeep Banerjee <subhadeep762@gmail.com>
2020-07-20 04:03:13 +0530

    Updated

--- Python/3. Strings/Find a String.py
+++ Python/3. Strings/Find a String.py
@@ -1,5 +1,9 @@
 def count_substring(string, sub_string):
-    return
+    cnt = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            cnt += 1
+    return cnt
 
 
 if __name__ == '__main__':
