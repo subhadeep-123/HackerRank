d11228ad5189b85bd020a9d466d02cbcd63b2142
Subhadeep Banerjee <subhadeep762@gmail.com>
2020-07-20 22:33:18 +0530

    Updated

--- Python/3. Strings/Designer Door Mat.py
+++ Python/3. Strings/Designer Door Mat.py
@@ -5,3 +5,8 @@ for i in range(1, N, 2):
 print('WELCOME'.center(M, '-'))
 for i in range(N-2, -1, -2):
     print((i*'.|.').center(M, '-'))
+
+# Solution2
+n, m = map(int, input().split())
+pattern = [(i*'.|.').center(m, '-') for i in range(1, n, 2)]
+print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
