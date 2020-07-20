e22faf919ed5fbba061dd2269d9a9ea582fd2b19
Subhadeep Banerjee <subhadeep762@gmail.com>
2020-07-20 20:54:00 +0530

    Updated

--- Python/3. Strings/String Validators.py
+++ Python/3. Strings/String Validators.py
@@ -1,6 +1,11 @@
-s = input()
-print(any([char.isalnum() for char in s]))
-print(any([char.isalpha() for char in s]))
-print(any([char.isdigit() for char in s]))
-print(any([char.islower() for char in s]))
-print(any([char.isupper() for char in s]))
+def soln(s):
+    print(any([char.isalnum() for char in s]))
+    print(any([char.isalpha() for char in s]))
+    print(any([char.isdigit() for char in s]))
+    print(any([char.islower() for char in s]))
+    print(any([char.isupper() for char in s]))
+
+
+if __name__ == '__main__':
+    s = input()
+    soln(s)
