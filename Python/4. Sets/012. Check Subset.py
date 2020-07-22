3b2883c5425c4186f6f9d7c7e65cf123315ee460
Subhadeep Banerjee <subhadeep762@gmail.com>
2020-07-23 01:06:28 +0530

    Updated

--- Python/4. Sets/012. Check Subset.py
+++ Python/4. Sets/012. Check Subset.py
@@ -1,6 +1,14 @@
+# solution 1
 for i in range(int(input())):
     a = int(input())
     A = set(map(int, input().split()))
     b = int(input())
     B = set(map(int, input().split()))
+    print(A.issubset(B))
+
+
+# Solution2
+for i in range(int(input())):
+    a, A = int(input()), set(map(int, input().split()))
+    b, B = int(input()), set(map(int, input().split()))
     print(A.issubset(B))
