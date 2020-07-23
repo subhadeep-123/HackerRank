23b3452072256388d4b9a97a7fbf420b107b8bf6
Subhadeep Banerjee <subhadeep762@gmail.com>
2020-07-24 01:23:45 +0530

    Updated

---
+++ Python/7. Collections/03. Collections.namedtuple().py
@@ -0,0 +1,11 @@
+from collections import namedtuple
+n = int(input())
+fields = input().split()
+
+total_marks = 0
+for _ in range(n):
+    students = namedtuple('students', fields)
+    MARKS, CLASS, NAME, ID = input().split()
+    student = students(MARKS, CLASS, NAME, ID)
+    total_marks += int(student.MARKS)
+print('{:.2f}'.format(total_marks/n))
