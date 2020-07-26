308d93776bdec5f5be9eda3b82c602d0b90d8051
Subhadeep Banerjee <subhadeep762@gmail.com>
2020-07-27 03:04:01 +0530

    Updated

---
+++ 30 Days of Code/Day 9 Recursion 3.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n == 1:
+        return n
+    else:
+        return n * factorial(n-1)
+
+
+if __name__ == '__main__':
+    num = int(input())
+    res = factorial(num)
+    print(res)
