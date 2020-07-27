4de3f9eaf3ecd3eeb1ed22898a1b0a51495d14a9
Subhadeep Banerjee <subhadeep762@gmail.com>
2020-07-27 05:48:41 +0530

    Updated

--- Python/011. Built-ins/06. ginortS.py
+++ Python/011. Built-ins/06. ginortS.py
@@ -1,7 +1,7 @@
-"""
-All sorted lowercase letters are ahead of uppercase letters.
-All sorted uppercase letters are ahead of digits.
-All sorted odd digits are ahead of sorted even digits.
-"""
+# All sorted lowercase letters are ahead of uppercase letters.
+# All sorted uppercase letters are ahead of digits.
+# All sorted odd digits are ahead of sorted even digits.
+
+
 print(*sorted(input(), key=lambda c: (c.isdigit() -
                                       c.islower(), c in '02468', c)), sep='')
